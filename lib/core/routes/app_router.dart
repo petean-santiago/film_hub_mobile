e2b21844841e9a49b8fb79bfd989bc805f2a3755
Santiago Petean <santiagopeteanit@gmail.com>
2025-11-04 11:03:49 +0100

    feat(structure): add clean project architecture, riverpod setup, and go_router configuration

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,13 @@
+import 'package:film_hub/presentation/view/home_view.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: 'home',
+      builder: (context, state) => const HomeView(),
+    ),
+  ],
+);
