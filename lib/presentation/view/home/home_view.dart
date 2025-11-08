1b2a3b02e028482e79e76b0b9964a35fd1923599
Santiago Petean <santiagopeteanit@gmail.com>
2025-11-08 23:52:51 +0100

    feat(home): implement scroll-hide app bar and add movie carousel package

--- lib/presentation/view/home/home_view.dart
+++ lib/presentation/view/home/home_view.dart
@@ -1,29 +1,96 @@
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final List<String> filters = ["Trending", "Popular", "Top Rated", "Upcoming"];
+
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("FilmHub")),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: GridView.builder(
-          itemCount: 20, 
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,        
-            crossAxisSpacing: 8,     
-            mainAxisSpacing: 8,       
-            childAspectRatio: 0.65,   
+      backgroundColor: Colors.black,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.black,
+            floating: true,
+            snap: true,
+            elevation: 0,
+            title: const Text(
+              "FilmHub",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(50),
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: SizedBox(
+                  height: 50,
+                  child: ListView.separated(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      final isSelected = selectedIndex == index;
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() => selectedIndex = index);
+                        },
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 200),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 14,
+                            vertical: 8,
+                          ),
+                          decoration: BoxDecoration(
+                            color: isSelected ? Colors.white : Colors.white12,
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                          child: Center(
+                            child: Text(
+                              filters[index],
+                              style: TextStyle(
+                                color: isSelected ? Colors.black : Colors.white,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (_, __) => const SizedBox(width: 8),
+                    itemCount: filters.length,
+                  ),
+                ),
+              ),
+            ),
           ),
-          itemBuilder: (context, index) {
-            return Image.network('https://m.media-amazon.com/images/I/811lT7khIrL._AC_UF894,1000_QL80_.jpg');
-          },
-        ),
+
+          SliverPadding(
+            padding: const EdgeInsets.only(left: 12, right: 12, bottom: 100),
+            sliver: SliverGrid(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+                childAspectRatio: 0.65,
+              ),
+              delegate: SliverChildBuilderDelegate((context, index) {
+                return Image.network(
+                  'https://m.media-amazon.com/images/I/811lT7khIrL._AC_UF894,1000_QL80_.jpg',
+                  fit: BoxFit.cover,
+                );
+              }, childCount: 20),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
-
