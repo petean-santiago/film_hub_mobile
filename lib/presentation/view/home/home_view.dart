be3e7490d6ccef9ddcc40cd1573bf9db74bfaa33
Santiago Petean <santiagopeteanit@gmail.com>
2025-12-09 14:33:18 +0100

    Add README screenshots and fix overflow issue in home_view

--- lib/presentation/view/home/home_view.dart
+++ lib/presentation/view/home/home_view.dart
@@ -71,7 +71,7 @@ class _HomeViewState extends State<HomeView> {
                   MovieCarousel(key: sectionKeys["now_playing"]),
 
                   Transform.translate(
-                    offset: const Offset(0, 0),
+                    offset: const Offset(0, -150),
                     child: Column(
                       children: [
                         ListView.builder(
@@ -93,7 +93,7 @@ class _HomeViewState extends State<HomeView> {
                     ),
                   ),
 
-                  const SizedBox(height: 105),
+                  const SizedBox(height: 0),
                 ],
               ),
             ),
